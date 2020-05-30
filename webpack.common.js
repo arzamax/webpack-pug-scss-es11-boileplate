91cbf29d0ca51e88992c15b951ce51bbd1f2dda0
Maxim Arzamastsev <arzamastsev@adv.ru>
2020-05-30 18:48:28 +0400

    Added babel object-rest-spread plugin

--- webpack.common.js
+++ webpack.common.js
@@ -40,7 +40,10 @@ module.exports = {
         loader: 'babel-loader',
         options: {
           presets: ['@babel/preset-env'],
-          plugins: ['@babel/plugin-proposal-class-properties'],
+          plugins: [
+            '@babel/plugin-proposal-object-rest-spread',
+            '@babel/plugin-proposal-class-properties',
+          ],
         },
       },
       {
